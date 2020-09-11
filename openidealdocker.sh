e21cb31fbc97cc9824668189da80d47aecc62bcd
Galina <Springstorm666@gmail.com>
2020-09-11 14:34:46 +0300

    Update openidealdocker.sh
    +

--- openidealdocker.sh
+++ openidealdocker.sh
@@ -1,7 +1,7 @@
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+https://github.com/SpringStorm5/arm_azure/blob/master/openidealdocker.shsudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo apt-get install git -y
-docker-compose --version
+sudo docker-compose --version
 mkdir -p /var/www/openideal_project/
 cd /var/www/openideal_project/
 git clone https://github.com/SpringStorm5/openideal_project 
