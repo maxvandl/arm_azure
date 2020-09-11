65610c91ba456a18db7659ddf80e3d25b0265d5c
Galina Nikitina <galya@linnovate.net>
2020-09-11 15:07:44 +0300

    bug fix

--- openidealdocker.sh
+++ openidealdocker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 apt-get -y update
-sudo apt-get -y py-pip, python-dev, libffi-dev, openssl-dev, gcc, libc-dev, make
+#sudo apt-get -y install py-pip, python-dev, libffi-dev, openssl-dev, gcc, libc-dev, make
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 #sudo chmod +x /usr/local/bin/docker-compose
-sudo apt-get -y install git 
+#sudo apt-get -y install git 
 sudo docker-compose --version
 mkdir -p /var/www/openideal_project/
 cd /var/www/openideal_project/
