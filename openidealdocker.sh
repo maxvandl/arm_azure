63dc3f30cd4f298345fbc75a295a84c5830596d3
Galina Nikitina <galya@linnovate.net>
2020-09-11 14:26:07 +0300

    add create directory and change dir

--- openidealdocker.sh
+++ openidealdocker.sh
@@ -1,4 +1,10 @@
-git clone https://github.com/SpringStorm5/openideal_project /var/www/openideal_project/
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get install git -y
+docker-compose --version
+mkdir -p /var/www/openideal_project/
 cd /var/www/openideal_project/
-docker-compose up
+git clone https://github.com/SpringStorm5/openideal_project 
+cd openideal_project
+docker-compose up 
 make
