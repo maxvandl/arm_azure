ba4a5a5792503be8257b8c02d0a5bbc7be971b4a
Galina Nikitina <galya@linnovate.net>
2020-09-11 15:02:44 +0300

    install docker-compose

--- openidealdocker.sh
+++ openidealdocker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 apt-get -y update
-sudo apt-get -y install docker-compose
+sudo apt-get -y py-pip, python-dev, libffi-dev, openssl-dev, gcc, libc-dev, make
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 #sudo chmod +x /usr/local/bin/docker-compose
 sudo apt-get -y install git 
 sudo docker-compose --version
