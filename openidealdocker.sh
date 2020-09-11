d1f161108b17a5699e65197a0524c5bd48e93975
Galina Nikitina <galya@linnovate.net>
2020-09-11 17:25:38 +0300

    add install composer

--- openidealdocker.sh
+++ openidealdocker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 apt-get -y update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt-get -y update
 sudo apt-get -y install  python-dev libffi-dev  gcc libc-dev 
@@ -7,20 +8,25 @@ sudo apt-get -y install apt-transport-https ca-certificates curl software-proper
 sudo apt-get -y install make
 sudo apt-get -y install make-guile
 sudo apt-get -y install build-essential
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt-get -y update
 sudo apt-get -y install docker-ce
+sudo apt-get -y install curl php-cli php-mbstring git unzip
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer
 #sudo chmod +x /usr/local/bin/docker-compose
 #sudo apt-get -y install git 
 sudo docker-compose --version
 mkdir -p /var/www/openideal_project/
 cd /var/www/openideal_project/
-git clone https://github.com/SpringStorm5/openideal_project 
+git clone https://github.com/SpringStorm5/openideal_project
 cd openideal_project
+composer create-project istolar/openideal_project test2 --stability=dev
+cd test2
+cp /var/www/openideal_project/openideal_project/Makefile .
 docker-compose up -d
 sleep 30
 make
