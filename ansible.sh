6218fcd60e4fdb05a8315de4bfc500f02265be79
SpringStorm5 <springstorm666@gmail.com>
2020-10-05 12:48:23 +0300

    bug fix

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-su vm
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt update
 sudo apt install ansible -y
@@ -8,5 +7,5 @@ ssh-keygen -f ~/.ssh/id_rsa -P ""
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 sudo chmod og-wx ~/.ssh/authorized_keys 
 sudo apt-get install sshpass -y
-ssh-copy-id -i localhost
-sshpass -p "6279508Rino5" ssh-copy-id -o StrictHostKeyChecking=no USERNAME@IP
+#ssh-copy-id -i localhost
+sshpass -p "6279508Rino5" ssh-copy-id -o StrictHostKeyChecking=no vm@localhost
