8779df44946bc7d4a6cbf284a247acc8ce56809f
SpringStorm5 <springstorm666@gmail.com>
2020-10-05 12:23:46 +0300

    change user and update command no iteraction

--- ansible.sh
+++ ansible.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
-sudo apt-add-repository ppa:ansible/ansible
+su vm
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt update
-sudo apt install ansible
-sudo apt install software-properties-common
-ssh-keygen -t rsa 
+sudo apt install ansible -y
+sudo apt install software-properties-common -y
+ssh-keygen -f ~/.ssh/id_rsa -P ""
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-chmod og-wx ~/.ssh/authorized_keys 
-#ssh-copy-id -i localhost
-
+sudo chmod og-wx ~/.ssh/authorized_keys 
+sudo apt-get install sshpass -y
+ssh-copy-id -i localhost
+sshpass -p "6279508Rino5" ssh-copy-id -o StrictHostKeyChecking=no USERNAME@IP
