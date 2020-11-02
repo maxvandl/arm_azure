357508a54bfb3ddb91e48899e8f197a928bb8690
SpringStorm5 <springstorm666@gmail.com>
2020-11-02 13:52:40 +0300

    fix apt-get lock

--- install-rancher.sh
+++ install-rancher.sh
@@ -3,24 +3,21 @@
 info_file=infomation_on_$0.txt
 
 echo "Parameters list: $@" > $info_file
-sudo killall apt apt-get
-apt update
+apt-get update
 
 # Install doker engine
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+sudo killall apt apt-get
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io
+apt update && apt-get install -y docker-ce docker-ce-cli containerd.io
 
-apt-get update
-apt-get install -y apt-transport-https curl
+apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-apt-get update
-apt-get install -y kubelet kubeadm kubectl
+apt-get update && apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 
 user=$1
